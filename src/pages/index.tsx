30aa81795453ba938a7552a9ffd4a3dc2796ee13
scisteps <170177246+scisteps@users.noreply.github.com>
2026-09-05 12:38:33 -0800

    tryharder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,62 @@
-import { Head } from "@/components";
-import { HomeLayout } from "@/layout/home";
-import { HomePageProps } from "@/layout/home/types";
-import { homeService } from "@/services/home";
-import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import { Head } from "@/components"
+import { HomeLayout } from "@/layout/home"
+import { HomePageProps } from "@/layout/home/types"
+import { GetServerSideProps, InferGetServerSidePropsType } from "next"
+
+import db from "public/database.json"
+
+const products = db.products.map(product => ({
+  ...product,
+  colors: (product.colors || []).map(color => ({
+    color: color.value
+  }))
+}))
 
 export const getServerSideProps: GetServerSideProps<HomePageProps> = async () => {
-  const { data } = await homeService.find(
+  const arrivals = db.home.arrivals
+    .map(id => products.find(product => product.id === id))
+    .filter(
+      (product): product is typeof products[number] =>
+        product !== undefined
+    )
 
+  const tranding = db.home.trandings.map(tranding => ({
+    id: tranding.id,
+    title: tranding.title,
+    products: products.filter(product =>
+      tranding.productIds.includes(product.id)
+    )
+  }))
+
+  const tshirts = products.filter(
+    product => product.brand?.toLowerCase() === "tshirts"
   )
+
   return {
     props: {
-      data
+      data: {
+        above_fold: db.home.above_fold,
+        tranding,
+        collection_banner: db.home.collection_banner,
+        arrivals,
+        season_sale: db.home.season_sale,
+        collections: db.home.collections,
+        tshirts
+      }
     }
   }
 }
 
-export default function HomePage (props: InferGetServerSidePropsType<typeof getServerSideProps>) {
-
-  
+export default function HomePage (
+  props: InferGetServerSidePropsType<typeof getServerSideProps>
+) {
   return (
     <>
-      <Head title="Paulyna | Home" description="Home page" />
+      <Head
+        title="Paulyna | Home"
+        description="Home page"
+      />
+
       <HomeLayout {...props} />
     </>
   )
